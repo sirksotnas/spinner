9e4772031a0335a61c175034c368636afafbbbe8
Kris Santos <kris.santos101@gmail.com>
2023-04-27 09:56:42 -0600

    updated spinner to match demo

--- spinner1.js
+++ spinner1.js
@@ -16,5 +16,21 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ');
+}, 1500);
+
+setTimeout(() => {
   process.stdout.write('\r|   \n');
-}, 1000);
+}, 1700);
